772eb730138d17501f60b5a239c7ba0fc3bb676c
obindev <36115112+obindev@users.noreply.github.com>
2024-10-09 16:59:28 -0500

    change3

--- assets/front.js
+++ assets/front.js
@@ -9,12 +9,12 @@ function drawProducts() {
             <div data-productId='${element.productId}'>
                 <img src='${element.image}'>
                 <h3>${element.name}</h3>
-                <p>price: ${currencySymbol}${element.price}</p>
+                <p>Price: ${currencySymbol}${convertCurrency(element.price)}</p>
                 <button class="add-to-cart">Add to Cart</button>
             </div>
         `;
     });
-    // use innerHTML so that products only drawn once
+    // use innerHTML so that products are only drawn once
     productList.innerHTML = productItems;
 }
 
@@ -29,16 +29,15 @@ function drawCart() {
         cartItems += `
             <div data-productId='${element.productId}'>
                 <h3>${element.name}</h3>
-                <p>price: ${currencySymbol}${element.price}</p>
-                <p>quantity: ${element.quantity}</p>
-                <p>total: ${currencySymbol}${itemTotal}</p>
+                <p>Price: ${currencySymbol}${convertCurrency(element.price)}</p>
+                <p>Quantity: ${element.quantity}</p>
+                <p>Total: ${currencySymbol}${convertCurrency(itemTotal)}</p>
                 <button class="qup">+</button>
                 <button class="qdown">-</button>
-                <button class="remove">remove</button>
+                <button class="remove">Remove</button>
             </div>
         `;
     });
-    // use innerHTML so that cart products only drawn once
     cart.length
         ? (cartList.innerHTML = cartItems)
         : (cartList.innerHTML = 'Cart Empty');
@@ -49,11 +48,10 @@ function drawCheckout() {
     let checkout = document.querySelector('.cart-total');
     checkout.innerHTML = '';
 
-    // run cartTotal() from script.js
-    let cartSum = cartTotal();
+    let cartSum = cartTotal(); // run cartTotal() from script.js
 
     let div = document.createElement('div');
-    div.innerHTML = `<p>Cart Total: ${currencySymbol}${cartSum}`;
+    div.innerHTML = `<p>Cart Total: ${currencySymbol}${cartSum}</p>`;
     checkout.append(div);
 }
 
@@ -72,51 +70,39 @@ document.querySelector('.products').addEventListener('click', (e) => {
 
 // Event delegation used to support dynamically added cart items
 document.querySelector('.cart').addEventListener('click', (e) => {
-    // Helper nested higher order function to use below
-    // Must be nested to have access to the event target
-    // Takes in a cart function as an agrument
     function runCartFunction(fn) {
         let productId = e.target.parentNode.getAttribute('data-productId');
         productId *= 1;
         for (let i = cart.length - 1; i > -1; i--) {
             if (cart[i].productId === productId) {
-                let productId = cart[i].productId;
                 fn(productId);
             }
         }
-        // force cart and checkout redraw after cart function completes
         drawCart();
         drawCheckout();
     }
 
-    // check the target's class and run function based on class
     if (e.target.classList.contains('remove')) {
-        // run removeProductFromCart() from script.js
         runCartFunction(removeProductFromCart);
     } else if (e.target.classList.contains('qup')) {
-        // run increaseQuantity() from script.js
         runCartFunction(increaseQuantity);
     } else if (e.target.classList.contains('qdown')) {
-        // run decreaseQuantity() from script.js
         runCartFunction(decreaseQuantity);
     }
 });
 
+// Payment functionality
 document.querySelector('.pay').addEventListener('click', (e) => {
     e.preventDefault();
 
-    // Get input cash received field value, set to number
     let amount = document.querySelector('.received').value;
     amount *= 1;
 
-    // Set cashReturn to return value of pay()
     let cashReturn = pay(amount);
 
     let paymentSummary = document.querySelector('.pay-summary');
     let div = document.createElement('div');
 
-    // If total cash received is greater than cart total thank customer
-    // Else request additional funds
     if (cashReturn >= 0) {
         div.innerHTML = `
             <p>Cash Received: ${currencySymbol}${amount}</p>
@@ -124,68 +110,45 @@ document.querySelector('.pay').addEventListener('click', (e) => {
             <p>Thank you!</p>
         `;
     } else {
-        // reset cash field for next entry
         document.querySelector('.received').value = '';
         div.innerHTML = `
             <p>Cash Received: ${currencySymbol}${amount}</p>
-            <p>Remaining Balance: ${cashReturn}$</p>
-            <p>Please pay additional amount.</p>
+            <p>Remaining Balance: ${currencySymbol}${cashReturn}</p>
+            <p>Please pay the remaining amount.</p>
             <hr/>
         `;
     }
-
     paymentSummary.append(div);
 });
 
-/* Standout suggestions */
-/* Begin remove all items from cart */
-// function dropCart(){
-//     let shoppingCart = document.querySelector('.empty-btn');
-//     let div = document.createElement("button");
-//     div.classList.add("empty");
-//     div.innerHTML =`Empty Cart`;
-//     shoppingCart.append(div);
-// }
-// dropCart();
+// Currency converter
+function currencyBuilder() {
+    let currencyPicker = document.querySelector('.currency-selector');
+    let select = document.createElement('select');
+    select.classList.add('currency-select');
+    select.innerHTML = `
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="YEN">YEN</option>
+    `;
+    currencyPicker.append(select);
+}
+currencyBuilder();
 
-// document.querySelector('.empty-btn').addEventListener('click', (e) => {
-//     if (e.target.classList.contains('empty')){
-//         emptyCart();
-//         drawCart();
-//         drawCheckout();
-//     }
-// })
-/* End all items from cart */
-
-/* Begin currency converter */
-// function currencyBuilder(){
-//     let currencyPicker = document.querySelector('.currency-selector');
-//     let select = document.createElement("select");
-//     select.classList.add("currency-select");
-//     select.innerHTML = `<option value="USD">USD</option>
-//                         <option value="EUR">EUR</option>
-//                         <option value="YEN">YEN</option>`;
-//     currencyPicker.append(select);
-// }
-// currencyBuilder();
-
-// document.querySelector('.currency-select').addEventListener('change', function handleChange(event) {
-//     switch(event.target.value){
-//         case 'EUR':
-//             currencySymbol = '€';
-//             break;
-//         case 'YEN':
-//             currencySymbol = '¥';
-//             break;
-//         default:
-//             currencySymbol = '$';
-//             break;
-//      }
-
-//     currency(event.target.value);
-//     drawProducts();
-//     drawCart();
-//     drawCheckout();
-// });
-/* End currency converter */
-/* End standout suggestions */
+document.querySelector('.currency-select').addEventListener('change', function handleChange(event) {
+    switch (event.target.value) {
+        case 'EUR':
+            currencySymbol = '€';
+            break;
+        case 'YEN':
+            currencySymbol = '¥';
+            break;
+        default:
+            currencySymbol = '$';
+            break;
+    }
+    currency(event.target.value);
+    drawProducts();
+    drawCart();
+    drawCheckout();
+});
--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,143 @@
-/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+// Array of product objects
+let products = [
+  {
+    name: "Carton of Cherries",
+    price: 4,
+    quantity: 0, // quantity starts at zero in cart
+    productId: 100,
+    image: "./images/cherry.jpg"
+  },
+  {
+    name: "Carton of Strawberries",
+    price: 5,
+    quantity: 0,
+    productId: 101,
+    image: "./images/strawberry.jpg"
+  },
+  {
+    name: "Bag of Oranges",
+    price: 10,
+    quantity: 0,
+    productId: 102,
+    image: "./images/orange.jpg"
+  }
+];
 
-/* Create 3 or more product objects using object literal notation 
-   Each product should include five properties
-   - name: name of product (string)
-   - price: price of product (number)
-   - quantity: quantity in cart should start at zero (number)
-   - productId: unique id for the product (number)
-   - image: picture of product (url string)
-*/
+// Empty array to hold cart items
+let cart = [];
 
-/* Images provided in /images folder. All images from Unsplash.com
-   - cherry.jpg by Mae Mu
-   - orange.jpg by Mae Mu
-   - strawberry.jpg by Allec Gomes
-*/
+// Currency rates (relative to USD)
+let currencyRates = {
+  USD: 1,
+  EUR: 0.85,
+  YEN: 110.53
+};
 
-/* Declare an empty array named cart to hold the items in the cart */
+let currentCurrency = 'USD';
 
-/* Create a function named addProductToCart that takes in the product productId as an argument
-  - addProductToCart should get the correct product based on the productId
-  - addProductToCart should then increase the product's quantity
-  - if the product is not already in the cart, add it to the cart
-*/
+/**
+ * Add a product to the cart
+ */
+function addProductToCart(productId) {
+  let product = products.find(item => item.productId === productId);
+  let cartItem = cart.find(item => item.productId === productId);
 
-/* Create a function named increaseQuantity that takes in the productId as an argument
-  - increaseQuantity should get the correct product based on the productId
-  - increaseQuantity should then increase the product's quantity
-*/
-
-/* Create a function named decreaseQuantity that takes in the productId as an argument
-  - decreaseQuantity should get the correct product based on the productId
-  - decreaseQuantity should decrease the quantity of the product
-  - if the function decreases the quantity to 0, the product is removed from the cart
-*/
-
-/* Create a function named removeProductFromCart that takes in the productId as an argument
-  - removeProductFromCart should get the correct product based on the productId
-  - removeProductFromCart should update the product quantity to 0
-  - removeProductFromCart should remove the product from the cart
-*/
-
-/* Create a function named cartTotal that has no parameters
-  - cartTotal should iterate through the cart to get the total cost of all products
-  - cartTotal should return the total cost of the products in the cart
-  Hint: price and quantity can be used to determine total cost
-*/
-
-/* Create a function called emptyCart that empties the products from the cart */
-
-/* Create a function named pay that takes in an amount as an argument
-  - amount is the money paid by customer
-  - pay will return a negative number if there is a remaining balance
-  - pay will return a positive number if money should be returned to customer
-  Hint: cartTotal function gives us cost of all the products in the cart  
-*/
-
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
-
-
-/* The following is for running unit tests. 
-   To fully complete this project, it is expected that all tests pass.
-   Run the following command in terminal to run tests
-   npm run test
-*/
-
-module.exports = {
-   products,
-   cart,
-   addProductToCart,
-   increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
-   /* Uncomment the following line if completing the currency converter bonus */
-   // currency
+  if (cartItem) {
+    cartItem.quantity++;
+  } else {
+    product.quantity = 1;
+    cart.push(product);
+  }
 }
+
+/**
+ * Increase product quantity in the cart
+ */
+function increaseQuantity(productId) {
+  let cartItem = cart.find(item => item.productId === productId);
+  if (cartItem) {
+    cartItem.quantity++;
+  }
+}
+
+/**
+ * Decrease product quantity or remove from cart if it reaches 0
+ */
+function decreaseQuantity(productId) {
+  let cartItem = cart.find(item => item.productId === productId);
+  if (cartItem) {
+    cartItem.quantity--;
+    if (cartItem.quantity === 0) {
+      removeProductFromCart(productId);
+    }
+  }
+}
+
+/**
+ * Remove product from cart
+ */
+function removeProductFromCart(productId) {
+  cart = cart.filter(item => item.productId !== productId);
+}
+
+/**
+ * Calculate the total cost of products in the cart in the current currency
+ */
+function cartTotal() {
+  let total = 0;
+  cart.forEach(item => {
+    total += item.price * item.quantity;
+  });
+  return convertCurrency(total);
+}
+
+/**
+ * Convert cart total to the selected currency
+ */
+function convertCurrency(amount) {
+  return (amount * currencyRates[currentCurrency]).toFixed(2);
+}
+
+/**
+ * Empty the cart
+ */
+function emptyCart() {
+  cart = [];
+}
+
+/**
+ * Pay the required amount
+ */
+let totalPaid = 0;
+
+function pay(amount) {
+  totalPaid += amount;
+  let totalCost = cartTotal();
+  let difference = totalPaid - totalCost;
+
+  if (difference >= 0) {
+    totalPaid = 0;
+  }
+  return difference;
+}
+
+/**
+ * Set the currency for the store and recalculate product prices
+ */
+function currency(currencyCode) {
+  currentCurrency = currencyCode;
+}
+
+/* The following is for running unit tests */
+module.exports = {
+  products,
+  cart,
+  addProductToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeProductFromCart,
+  cartTotal,
+  pay,
+  emptyCart,
+  currency // Uncommented for currency converter functionality
+};
